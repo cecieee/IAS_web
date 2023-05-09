a1172381a05ba01769aa2961d6c9f91c9f607d16
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2023-05-09 20:02:42 +0530

    page loading animation added: events, execom, home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,19 +6,19 @@ function Home() {
       <div className="homebody">
         <div className='container-fluid mid'>
           <h1 className='heading1 row'>
-            <span >INDUSTRY</span><br />
-            <span className=' col-lg-8 col-sm-12'>APPLICATIONS</span>
-            <span className='span1  col-lg-4 col-sm-12'>SOCIETY</span>
+            <span data-aos="fade-up" >INDUSTRY</span><br />
+            <span data-aos="fade-up" className=' col-lg-8 col-sm-12'>APPLICATIONS</span>
+            <span data-aos="fade-up" className='span1  col-lg-4 col-sm-12'>SOCIETY</span>
           </h1>
           <h3 className='subheading'>
-            <span>IEEE Student Branch Chapter</span><br />
+            <span data-aos="fade-up" >IEEE Student Branch Chapter</span><br />
             <span>College of Engineering Chengannur</span></h3>
         </div>
 
       </div>
-      <div className='container-fluid'>
+      <div  className='container-fluid'>
         <div className="row">
-          <div className='col-lg-12'>
+          <div data-aos="fade-up" className='col-lg-12'>
             <div className="m-v-box">
               <div>
               <h1>MISSION</h1>
@@ -26,7 +26,7 @@ function Home() {
               <p>The prime objective of IEEE Industry Applications Society is to lead the way in the progress of science and technology by establishing a strong link between theoretical concepts and practical applications of electrical and electronic systems. The ultimate aim of this endeavor is to provide concrete benefits to humanity. IEEE IAS SBC CEC strives to promote sustainable and economical developments towards the society.</p>
             </div>
           </div>
-         <div className="col-lg-12">
+         <div data-aos="fade-up" className="col-lg-12">
          <div className="m-v-box">
             <div>
             <h1>VISION</h1>
