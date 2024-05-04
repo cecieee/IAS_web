13c8b4ce5ac848d8b41cc89cdc346d3ccce6c260
Harikrishna A <Harikichus2018@gmail.com>
2024-05-05 00:40:53 +0530

    Added About section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -71,6 +71,22 @@ function Home() {
         <div className="container-fluid">
           <div className="row">
             <div data-aos="fade-up" className="col-lg-12">
+              <div className="m-v-box" >
+                <div>
+                  <h1 >ABOUT</h1>
+                </div>
+                <h3>IEEE SB CEC</h3>
+                <p>
+                IEEE Student Branch College of Engineering Chengannur was formed in mid-1997, with the goal of keeping the students in touch with advancing technology. It is equipped with an IEEE library that was inaugurated in December 1999 and currently functions as the headquarters of IEEE SB CEC. The Student Branch comes under the Kochi Hub of IEEE Kerala Section and is one of the most active Student Branches in the country. IEEE SB CEC has made numerous contributions to its student community that has received widespread recognition across the globe and is the first Student Branch in the Asia-Pacific Region to establish a Women in Engineering Affinity Group.
+                </p>
+                <h3>IEEE IAS SBC CEC</h3>
+                <p>
+                IEEE Student Branch College of Engineering Chengannur was formed in mid-1997, with the goal of keeping the students in touch with advancing technology. It is equipped with an IEEE library that was inaugurated in December 1999 and currently functions as the headquarters of IEEE SB CEC. The Student Branch comes under the Kochi Hub of IEEE Kerala Section and is one of the most active Student Branches in the country. IEEE SB CEC has made numerous contributions to its student community that has received widespread recognition across the globe and is the first Student Branch in the Asia-Pacific Region to establish a Women in Engineering Affinity Group.
+                </p>
+              </div>
+            </div>
+            
+            <div data-aos="fade-up" className="col-lg-12">
               <div className="m-v-box">
                 <div>
                   <h1>MISSION</h1>
